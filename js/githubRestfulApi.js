a9e6205ff373e5f89f6df4ed1f08e8aa0f2ce047
Bilgehan Gecici <bilgehangecici@gmail.com>
2020-04-26 18:14:49 +0300

    Update githubRestfulApi.js

--- js/githubRestfulApi.js
+++ js/githubRestfulApi.js
@@ -1,54 +1,77 @@
-// Create a request.
 
-var request = new XMLHttpRequest();
+$(document).ready(function () {
 
-// Open a new connection with GET request on the URL endpoint.
+  $("#button").on('click', function (e) {
+    e.preventDefault();
 
-// You can change the username section below with yours.
-request.open('GET', 'https://api.github.com/users/Bgstatic/repos', true);
-
-request.onload = function () {
-
-  // Accessing JSON file.
-
-  var data = JSON.parse(this.response);
-
-  var statusHTML = '';
-
-  // Get indicated information for projects.
-  $.each(data, function (i, status) {
-    
-    var param = parseInt(i);
-    statusHTML += '<tr>';
-    statusHTML += '<td>' + (param + 1) + '</td>';
-    statusHTML += '<td>' + status.name + '</td>';
-    if (status.description == null) {
-
-      statusHTML += '<td>' + "Cannot find project description" + '</td>';
-
-    } else {
-
-      statusHTML += '<td>' + status.description + '</td>';
+    var username = document.getElementById("input").value;
+    if (!username) {
+      alert("Invalid input. Please check the username and try again.");
     }
 
-    statusHTML += "<td id=url_id><a href=" + status.html_url + " target = _blank>" + status.html_url + "</a></td>";
-
-    if (status.language == null) {
-
-      statusHTML += '<td>' + "Cannot find project language" + '</td>';
-
-    } else {
-
-      statusHTML += '<td>' + status.language + '</td>';
-    }
-
-    statusHTML += '</tr>';
+    getdata(username);
 
   });
 
-  $('tbody').html(statusHTML);
+});
+
+function getdata(username) {
+
+  // Create a request.
+  var request = new XMLHttpRequest();
+  
+  // Open a new connection with GET request on the URL endpoint.
+  // You can change the username section below with yours.
+  request.open('GET', 'https://api.github.com/users/' + username + '/repos', false);
+
+  request.onload = function () {
+
+    // Accessing JSON file.
+
+    var data = JSON.parse(this.response);
+
+    var statusHTML = '';
+
+    // Get indicated information for projects.
+    $.each(data, function (i, status) {
+
+      var param = parseInt(i);
+      statusHTML += '<tr>';
+      statusHTML += '<td>' + (param + 1) + '</td>';
+      statusHTML += '<td>' + status.name + '</td>';
+      if (status.description == null) {
+
+        statusHTML += '<td>' + "Cannot find project description" + '</td>';
+
+      } else {
+
+        statusHTML += '<td>' + status.description + '</td>';
+      }
+
+      statusHTML += "<td id=url_id><a href=" + status.html_url + " target = _blank>" + status.html_url + "</a></td>";
+
+      if (status.language == null) {
+
+        statusHTML += '<td>' + "Cannot find project language" + '</td>';
+
+      } else {
+
+        statusHTML += '<td>' + status.language + '</td>';
+      }
+
+      statusHTML += '</tr>';
+
+    });
+
+    $('tbody').html(statusHTML);
+  }
+
+  // Send request at the end.
+
+  request.send();
+
+
 }
 
-// Send request at the end.
 
-request.send();
+
